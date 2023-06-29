50918e7ded1d43bfcdaeefe1d848767793f396ea
Raghav Sharma <raghavs@adobe.com>
2023-06-29 08:59:54 +0530

    Added a main function

--- Main.hs
+++ Main.hs
@@ -1,6 +1,15 @@
 module Main where
 
 import JSON
+import Parser
 
 main :: IO ()
-main = undefined
+main = readFile "../input.json" >>= parseFile
+
+parseFile :: String -> IO ()
+parseFile s = 
+  case parsed of
+    Left (_, _, r) -> putStrLn $ "Parsing Failed. Remaining String: " ++ r
+    Right (a, _) -> print a
+  where
+    parsed = run parseJSON s
